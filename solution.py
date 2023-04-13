04d400d8d4481da2b04e299fb75e214ac7836c9e
maijesk <109813087+maijesk@users.noreply.github.com>
2023-04-13 18:35:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import numpy as np
 from statsmodels. stats.weightstats import ztest as ztest
 
 
-chat_id = 763683015 # Ваш chat ID, не меняйте название переменной
+chat_id = 700775408 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.02
